ef035b6b59f5aa02dceb3eca61c02bd06a82e724
Lanuvelza <ianlam1221@hotmail.com>
2021-01-14 20:46:21 +0000

    fetches the information from the API and prints out a short based description of the given breed name by the user

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,12 +2,12 @@ const request = require('request');
 
 request(`https://api.thecatapi.com/v1/breeds/search?q=${process.argv[2]}`, (error, response, body) => {
   if (error) {
-    console.log("Error:", error)
+    console.log("Error:", error);
   }
   if (body === "[]") {
-    console.log("breed not found"); 
+    console.log("breed not found");
   } else {
-    const data = JSON.parse(body); 
+    const data = JSON.parse(body);
     console.log(data[0].description);
   }
 });
